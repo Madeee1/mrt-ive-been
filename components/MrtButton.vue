63abb269d0de1a1855838cc697bed74a13517aa7
Madeee1 <madeee@duck.com>
2024-03-15 08:59:45 +0000

    feat: add button feature

---
+++ components/MrtButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button class="w-5 h-5 rounded-full outline outline-black" 
+        :class="{'bg-green-500': isVisited, 'bg-white': !isVisited}"
+        @click="isVisited=!isVisited">
+    </button>
+</template>
+<script>
+export default {
+	name: "MrtButton",
+	data() {
+		return {
+			isVisited: false,
+		};
+	},
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <p>test</p>
+    <MrtButton />
   </div>
 </template>
